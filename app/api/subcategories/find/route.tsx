d09c4601d56858114775728c30640ec0015df590
VuDance <nhuyedangnguyen@gmail.com>
2023-10-03 20:18:55 +0700

    update filter

--- app/api/subcategories/find/route.tsx
+++ app/api/subcategories/find/route.tsx
@@ -7,6 +7,7 @@ export async function GET(request: NextRequest) {
     const color = request.nextUrl.searchParams.getAll("color");
     const size = request.nextUrl.searchParams.getAll("size");
     const price = request.nextUrl.searchParams.getAll("price");
+    const orderBy = request.nextUrl.searchParams.get("orderBy");
 
     const priceNumbers = price.map((p) => parseFloat(p));
 
@@ -49,10 +50,45 @@ export async function GET(request: NextRequest) {
                 product: {
                   productVariants: {
                     some: {
-                      price:
-                        priceNumbers.length > 0
-                          ? { gt: Math.min(...priceNumbers) }
-                          : undefined,
+                      price: (() => {
+                        if (priceNumbers.length > 0) {
+                          if (priceNumbers.includes(1)) {
+                            return { lt: 300000 };
+                          } else if (priceNumbers.includes(2)) {
+                            return { gte: 300000, lte: 500000 };
+                          } else if (priceNumbers.includes(3)) {
+                            return { gt: 500000 };
+                          }
+                        } else {
+                          return {};
+                        }
+                      })(),
+
+                      // OR: [
+                      //   {
+                      //     price:
+                      //       priceNumbers.length > 0 ? priceNumbers.includes(1)
+                      //         ? { lt: 300000 } ? priceNumbers.includes(2)
+                      //         : {},
+                      //   },
+                      //   {
+                      //     price:
+                      //       priceNumbers.length > 0 && priceNumbers.includes(2)
+                      //         ? {
+                      //             gte: 300000,
+                      //             lte: 500000,
+                      //           }
+                      //         : {},
+                      //   },
+                      //   {
+                      //     price:
+                      //       priceNumbers.length > 0 && priceNumbers.includes(3)
+                      //         ? {
+                      //             gt: 500000,
+                      //           }
+                      //         : {},
+                      //   },
+                      // ],
                     },
                   },
                 },
@@ -79,6 +115,16 @@ export async function GET(request: NextRequest) {
         ],
       },
     });
+    if (orderBy === "asc") {
+      listSub[0].products.sort(
+        (a, b) => a.product.minPrice - b.product.minPrice
+      );
+    }
+    if (orderBy === "desc") {
+      listSub[0].products.sort(
+        (a, b) => b.product.minPrice - a.product.minPrice
+      );
+    }
 
     return NextResponse.json({ data: listSub[0] });
   } catch (error: any) {
