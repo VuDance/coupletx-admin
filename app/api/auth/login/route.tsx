c48f4961e3fb9c143207d4908def80bdde19bbde
VuDance <96101080+VuDance@users.noreply.github.com>
2023-10-14 23:26:19 +0700

    Update route.tsx

--- app/api/auth/login/route.tsx
+++ app/api/auth/login/route.tsx
@@ -27,6 +27,6 @@ export async function POST(request: NextRequest) {
       { status: 200 }
     );
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 }
