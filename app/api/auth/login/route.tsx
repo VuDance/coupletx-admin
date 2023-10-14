446727649343652a7164fb3347b85612bb0b758b
VuDance <96101080+VuDance@users.noreply.github.com>
2023-10-14 23:57:26 +0700

    update login api

--- app/api/auth/login/route.tsx
+++ app/api/auth/login/route.tsx
@@ -1,31 +1,69 @@
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 import prisma from "@/lib/prismadb";
-import { signIn } from "next-auth/react";
+import { sendEmail } from "@/app/common/function/nodemailer";
 
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     const { email, password } = body;
 
-    const res: any = await signIn("credentials", {
-      email,
-      password,
-    });
-    if (res?.status === 401) {
+    if (!email || !password) {
       return NextResponse.json(
         {
-          message: "Invalid username or password",
+          message: "Email hoặc password không hợp lệ",
         },
         { status: 401 }
       );
     }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user || !user?.password) {
+      return NextResponse.json(
+        {
+          message: "Sai Email hoặc password",
+        },
+        { status: 401 }
+      );
+    }
+
+    const isCorrectPassword = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isCorrectPassword) {
+      return NextResponse.json(
+        {
+          message: "Sai Email hoặc password",
+        },
+        { status: 401 }
+      );
+    }
+    if (user.active === false) {
+      sendEmail({ email: user.email, userId: user.id, type: "VERIFY" });
+      return NextResponse.json(
+        {
+          message: "Vui lòng xác thực email",
+        },
+        { status: 401 }
+      );
+    }
+
+    
     return NextResponse.json(
       {
         message: "Đăng nhập thành công",
+        data:user,
       },
       { status: 200 }
     );
   } catch (error: any) {
-    return NextResponse.json({ error: error }, { status: 500 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
