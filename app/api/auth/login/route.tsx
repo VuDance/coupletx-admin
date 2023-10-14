d7ec90f3af2a167a307bd162a06428a56a669055
VuDance <96101080+VuDance@users.noreply.github.com>
2023-10-14 23:40:51 +0700

    Update route.tsx

--- app/api/auth/login/route.tsx
+++ app/api/auth/login/route.tsx
@@ -10,7 +10,6 @@ export async function POST(request: NextRequest) {
     const res: any = await signIn("credentials", {
       email,
       password,
-      redirect: false,
     });
     if (res?.status === 401) {
       return NextResponse.json(
